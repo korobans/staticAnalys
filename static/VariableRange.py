030f99096ea4050ec8001a994ce0c31d721a133e
koroban <138606321+korobans@users.noreply.github.com>
2023-09-11 22:03:12 +0300

    Add files via upload

--- static/VariableRange.py
+++ static/VariableRange.py
@@ -1,6 +1,6 @@
 from math import log, ceil
 import matplotlib.pyplot as plt
-from numpy import std, median, var
+from numpy import std, median, var, round
 
 
 class VariableRange:
@@ -21,7 +21,13 @@ class VariableRange:
             for j in range(len(self.cap_s) - 1):
                 if self.cap_s[j] <= i <= self.cap_s[j + 1]:
                     self.lower_s[j] += 1
+        self.cap_s = list(map(lambda x: round(x, 3), self.cap_s))
         return [self.cap_s, self.lower_s]
+
+    def moda(self):
+        self.variable_matrix()
+        pop = self.lower_s.index(max(self.lower_s))
+        return self.cap_s[pop] + self.dx / 2
 
     def graphic(self):
         self.variable_matrix()
@@ -33,11 +39,17 @@ class VariableRange:
         plt.bar(gr_arr, lower)
         plt.show()
 
-    def var(self):
+    def dispersion(self):
         return var(self.array)
 
     def median(self):
         return median(self.array)
 
-    def std(self):
+    def avg_deviation(self):
         return std(self.array)
+
+    def avg(self):
+        return sum(self.array) / self.n
+
+    def scope(self):
+        return self.r
--- static/dataAnalys.py
+++ static/dataAnalys.py
@@ -1,10 +1,25 @@
-import VariableRange as vr
+from VariableRange import VariableRange
 import csv
 
-s = []
-with open('Выборка.csv', newline='') as csvfile:
-    file = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in file:
-        s.append(float(row[0][1:-1].replace(',', '.')))
 
-vr.VariableRange(s).graphic()
+def csv_read(link, n=1):
+    s = []
+    n -= 1
+    with open(link, newline='') as csvfile:
+        file = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in file:
+            s.append(float(row[n]))
+        return s
+
+
+arr = csv_read('Выборки.csv', 1)
+s = VariableRange(arr).variable_matrix()
+for i in range(len(s[0]) - 1):
+    print(f'{s[0][i]} - {s[0][i + 1]} --> {s[1][i]}')
+print(VariableRange(arr).dispersion())
+print(VariableRange(arr).avg_deviation())
+print(VariableRange(arr).median())
+print(VariableRange(arr).moda())
+print(VariableRange(arr).avg())
+print(VariableRange(arr).scope())
+VariableRange(arr).graphic()
