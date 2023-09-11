fcf0a1452c7850725a177cbf076a8240701f7ae3
koroban <138606321+korobans@users.noreply.github.com>
2023-09-12 01:04:10 +0300

    Add files via upload

--- static/VariableRange.py
+++ static/VariableRange.py
@@ -1,9 +1,12 @@
+# импорт библиотек
 from math import log, ceil
 import matplotlib.pyplot as plt
 from numpy import std, median, var, round
 
 
+# объявление класса
 class VariableRange:
+    # стандартная функция с объявлением внутренних переменных, которые будут на весь класс распространяться
     def __init__(self, array):
         self.array = array
         self.n = len(array)
@@ -13,43 +16,61 @@ class VariableRange:
         self.cap_s = []
         self.lower_s = [0] * self.m
 
+    # функция вывода таблицы
     def variable_matrix(self):
+        # создание промежутков для таблицы
         for i in range(self.m + 1):
             self.cap_s.append(min(self.array) + self.dx * i)
 
+        # подсчет кол-ва попаданий точки в промежуток
         for i in self.array:
             for j in range(len(self.cap_s) - 1):
                 if self.cap_s[j] <= i <= self.cap_s[j + 1]:
                     self.lower_s[j] += 1
+        # форматирование промежутков до 3 знаков после точки
         self.cap_s = list(map(lambda x: round(x, 3), self.cap_s))
+        # ретурн епта
         return [self.cap_s, self.lower_s]
 
+    # вызов функции с табличкой, чтобы моду найти
     def moda(self):
         self.variable_matrix()
         pop = self.lower_s.index(max(self.lower_s))
         return self.cap_s[pop] + self.dx / 2
 
+    # функция для постройки графика
     def graphic(self):
+        # вызов таблицы
         self.variable_matrix()
+        # на всякий случай перевод кол-ва попаданий в числа с плавающей точкой
         lower = list(map(float, self.lower_s))
+        # последнее число нам не нужно, поэтому его мы удаляем
         gr_arr = self.cap_s[:-1]
+        # а к остальным прибавляем пол шага чтобы ровнее было (среднее отрезка)
         for i in range(len(gr_arr) - 1):
             gr_arr[i] += self.dx / 2
+        # назваем график, распределяем оси и выводим график
         plt.title("Распределение выборки")
         plt.bar(gr_arr, lower)
         plt.show()
 
+    # все функции что нужны для остального в umpy есть (мб и то, что выше есть, просто нахуй послали бы с такими завами)
+    # дисперсия
     def dispersion(self):
         return var(self.array)
 
+    # медиана
     def median(self):
         return median(self.array)
 
+    # среднее отклонение
     def avg_deviation(self):
         return std(self.array)
 
+    # среднее арифметическое
     def avg(self):
         return sum(self.array) / self.n
 
+    # размах
     def scope(self):
         return self.r
--- static/dataAnalys.py
+++ static/dataAnalys.py
@@ -1,21 +1,31 @@
+# импортируем объект и библиотеку чтобы таблицу считать
 from VariableRange import VariableRange
 import csv
 
 
+# ф-ия чтобы таблицу считать (в нее передаем название таблицы в папке локальной и номер столбца с выборкой)
 def csv_read(link, n=1):
     s = []
     n -= 1
+    # открываем табличку, на ньюлайн похуй, он у меня пустой вообще
     with open(link, newline='') as csvfile:
+        # в файл считываем нашу таблицу через ридер (делиметр=разделитель, куотечар не помню, похуй на него, в таблице кстати замените все запятые на точки, а то ахуеете)
         file = csv.reader(csvfile, delimiter=',', quotechar='|')
+        # записываем показания с таблицы в s
         for row in file:
             s.append(float(row[n]))
+            # угадайте что строчка ниже делает
         return s
 
 
+# смотрим что мы наделали, записываем выборку в переменную
 arr = csv_read('Выборки.csv', 1)
+# получаем таблицу вероятностей
 s = VariableRange(arr).variable_matrix()
+# красиво выводим ее
 for i in range(len(s[0]) - 1):
     print(f'{s[0][i]} - {s[0][i + 1]} --> {s[1][i]}')
+# ниже тест функций
 print(VariableRange(arr).dispersion())
 print(VariableRange(arr).avg_deviation())
 print(VariableRange(arr).median())
